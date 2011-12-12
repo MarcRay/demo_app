af5940d4ba46ce295dd2419a83e7675dd3c632c0
René <rl56762gmail.com>
2011-12-12 18:53:45 -0500

    Ajout de ressources a Micropost

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111212202743) do
+ActiveRecord::Schema.define(:version => 20111212235001) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "nom"
